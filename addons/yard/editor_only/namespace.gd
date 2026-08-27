5db9d3ee9f29259670ce627c5ea8d2992a8d9ba8
François de la Taste <canardbleu@gmail.com>
2026-08-27 14:10:57 +0200

    support Godot 4.8dev4

--- addons/yard/editor_only/namespace.gd
+++ addons/yard/editor_only/namespace.gd
@@ -10,7 +10,7 @@ const RegistryIO := preload("res://addons/yard/editor_only/registry_io.gd")
 const YardEditorCache := preload("res://addons/yard/editor_only/classes/yard_editor_cache.gd")
 const ClassUtils := preload("res://addons/yard/editor_only/classes/class_utils.gd")
 const EditorThemeUtils := preload("res://addons/yard/editor_only/classes/editor_theme_utils.gd")
-const FuzzySearch := preload("res://addons/yard/editor_only/classes/fuzzy_search.gd")
+const FuzzySearchYard := preload("res://addons/yard/editor_only/classes/fuzzy_search.gd")
 const AnyIcon := preload("res://addons/yard/editor_only/classes/any_icon.gd")
 const YardLogger := preload("res://addons/yard/editor_only/classes/yard_logger.gd")
 
