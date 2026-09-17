ecdd6d0b55f83618d39d702172b1f4e93fac5adf
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2026-09-17 15:05:01 +0200

    fix: main screen compat issue

--- addons/yard/plugin.gd
+++ addons/yard/plugin.gd
@@ -19,7 +19,7 @@ const EDITOR_INSPECTOR_PLUGIN := Namespace.EDITOR_INSPECTOR_PLUGIN
 var _registry_editor: RegistryEditor
 var _filesystem_create_context_menu_plugin: EditorContextMenuPlugin
 var _cached_plugin_name: String
-var _dock: Variant #EditorDock
+var _dock: Control #EditorDock
 
 
 func _init() -> void:
@@ -47,21 +47,24 @@ func _enter_tree() -> void:
 	_registry_editor = REGISTRY_EDITOR_SCENE.instantiate()
 
 	if Compat.is_engine_version_equal_or_newer(4, 8):
-		_dock = EditorDock.new()
-		_dock.title = "My Dock"
-		_dock.dock_icon = preload("res://addons/yard/editor_only/assets/yard.svg")
-		_dock.default_slot = EditorDock.DOCK_SLOT_MAIN_SCREEN
+		_dock = ClassDB.instantiate(&"EditorDock") as Node
+		_dock.set(&"title", _get_plugin_name())
+		_dock.set(&"dock_icon", preload("res://addons/yard/editor_only/assets/yard.svg"))
+		_dock.set(&"default_slot", ClassDB.class_get_integer_constant(&"EditorDock", &"DOCK_SLOT_MAIN_SCREEN"))
 		_dock.add_child(_registry_editor)
-		add_dock(_dock)
+		call(&"add_dock", _dock)
 	else:
 		EditorInterface.get_editor_main_screen().add_child(_registry_editor)
+		_make_visible(false)
 
 	_reimport_icons()
-	_make_visible(false)
 
 
 func _exit_tree() -> void:
-	if is_instance_valid(_registry_editor):
+	if is_instance_valid(_dock):
+		_dock.queue_free()
+		call(&"remove_dock", _dock)
+	elif is_instance_valid(_registry_editor):
 		_registry_editor.queue_free()
 
 	if is_instance_valid(_filesystem_create_context_menu_plugin):
